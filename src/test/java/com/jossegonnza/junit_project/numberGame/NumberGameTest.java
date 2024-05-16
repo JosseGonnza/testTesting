92bbce0be35029e8893af4fd97d57741d2e0a907
JosseGonnza <accion.quevedo@hotmail.com>
2024-05-16 18:06:34 +0100

    Refactoring code

--- src/test/java/com/jossegonnza/junit_project/numberGame/NumberGameTest.java
+++ src/test/java/com/jossegonnza/junit_project/numberGame/NumberGameTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class NumberGameTest {
 
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
     private NumberGame fizzBuzz;
 
     @Before
@@ -30,10 +32,10 @@ public class NumberGameTest {
     public void should_returnFizz_whenAMultipleOf3() {
 
         List<String> numbersList = fizzBuzz.getNumbers();
-        Assert.assertEquals("Fizz", numbersList.get(2));
-        Assert.assertEquals("Fizz", numbersList.get(5));
-        Assert.assertEquals("Fizz", numbersList.get(8));
-        Assert.assertEquals("Fizz", numbersList.get(11));
+        Assert.assertEquals(FIZZ, numbersList.get(2));
+        Assert.assertEquals(FIZZ, numbersList.get(5));
+        Assert.assertEquals(FIZZ, numbersList.get(8));
+        Assert.assertEquals(FIZZ, numbersList.get(11));
 
     }
 
@@ -41,10 +43,10 @@ public class NumberGameTest {
     public void should_returnBuzz_whenAMultipleOf5() {
 
         List<String> numbersList = fizzBuzz.getNumbers();
-        Assert.assertEquals("Buzz", numbersList.get(4));
-        Assert.assertEquals("Buzz", numbersList.get(9));
-        Assert.assertEquals("Buzz", numbersList.get(19));
-        Assert.assertEquals("Buzz", numbersList.get(24));
+        Assert.assertEquals(BUZZ, numbersList.get(4));
+        Assert.assertEquals(BUZZ, numbersList.get(9));
+        Assert.assertEquals(BUZZ, numbersList.get(19));
+        Assert.assertEquals(BUZZ, numbersList.get(24));
 
     }
 
@@ -52,10 +54,10 @@ public class NumberGameTest {
     public void should_returnFizzBuzz_whenAMultipleOf3And5() {
 
         List<String> numbersList = fizzBuzz.getNumbers();
-        Assert.assertEquals("FizzBuzz", numbersList.get(14));
-        Assert.assertEquals("FizzBuzz", numbersList.get(29));
-        Assert.assertEquals("FizzBuzz", numbersList.get(44));
-        Assert.assertEquals("FizzBuzz", numbersList.get(59));
+        Assert.assertEquals(FIZZ + BUZZ, numbersList.get(14));
+        Assert.assertEquals(FIZZ + BUZZ, numbersList.get(29));
+        Assert.assertEquals(FIZZ + BUZZ, numbersList.get(44));
+        Assert.assertEquals(FIZZ + BUZZ, numbersList.get(59));
 
     }
 
